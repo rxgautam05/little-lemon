048a57dd63e237aff71a6d869b8148520586c684
unknown <manish.kr.gautam05@gmail.com>
2023-07-30 16:44:57 +0530

    Initial commit

---
+++ src/components/pages/Home/index.js
@@ -0,0 +1,17 @@
+import Hero from './Hero';
+import WeekSpecials from './WeekSpecials';
+
+import OurStory from './OurStory';
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <WeekSpecials />
+    
+      <OurStory />
+    </>
+  );
+};
+
+export default Home;
